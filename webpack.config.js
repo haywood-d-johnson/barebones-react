96514624cc243cda54c3b465d84e802ff0ccce17
haywood-d-johnson <haywood.d.johnson@gmail.com>
2020-08-03 16:52:28 -0500

    config edit, added public and contents

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,9 @@
+const path = require("path");
+
 module.exports = {
-    entry: "./src/app.js"
+    entry: "./src/app.js",
+    output: {
+        path: path.join(__dirname, public),
+        filename: "bundle.js"
+    }
 };
