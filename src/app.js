06242591e78617d736d0ba45fd77843e5b411516
haywood-d-johnson <haywood.d.johnson@gmail.com>
2020-08-03 17:35:55 -0500

    added babelrc and webpack tweaks

--- src/app.js
+++ src/app.js
@@ -1,10 +1,10 @@
-import React from 'react';
+import React from "react";
 
-const App = () => {
+export const App = () => {
     return (
         <div>
             <h1>Barebones React</h1>
             <div>This is it...</div>
         </div>
     );
-}
+};
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
+import ReactDOM from "react-dom";
 
 import App from "./App";
 
 const appRoot = document.getElementById("app");
-ReactDOM.render(<App />, appRoot)
+ReactDOM.render(<App />, appRoot);
