29caf5eff252761e6289e9f0915a15f09dea2b2a
haywood-d-johnson <haywood.d.johnson@gmail.com>
2021-04-15 16:10:49 -0500

    fix: not rendering; update packages

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import App from "./App";
+import App from "./app";
 
 const appRoot = document.getElementById("root");
 ReactDOM.render(<App />, appRoot);
